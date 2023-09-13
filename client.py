c73f84b5d8bcd9a49581a8fbe04577788ccb09d0
Min <minjung10402@gmail.com>
2023-09-13 16:38:17 -0700

    checked for number input not in range

--- client.py
+++ client.py
@@ -1,7 +1,16 @@
 # Ask client for their name
-user_input = input("Name: ")
-output = "Client of " + user_input
+user_name = input("Name: ")
+output = "Client of " + user_name
 print(output)
 # Ask client to choose number between 1 to 100
-pick_integer = input("Choose a number between 1 and 100: ")
-print(pick_integer)
+def choose_int(): 
+    pick_int = input("Choose a number between 1 and 100: ")
+    # change data type from str to int
+    user_int = int(pick_int)
+    if user_int > 0 and user_int < 101:
+        print(user_int) 
+    else:
+        print("Error: Input interger is not between 1 and 100. Try again.")
+        choose_int()
+
+choose_int()
