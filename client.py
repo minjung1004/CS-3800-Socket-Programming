ea443d468caaf21403cf7edec525356ddbc5f134
Min <minjung10402@gmail.com>
2023-09-13 16:19:49 -0700

    choose number

---
+++ client.py
@@ -0,0 +1,7 @@
+# Ask client for their name
+user_input = input("Name: ")
+output = "Client of " + user_input
+print(output)
+# Ask client to choose number between 1 to 100
+pick_integer = input("Choose a number between 1 and 100: ")
+print(pick_integer)
