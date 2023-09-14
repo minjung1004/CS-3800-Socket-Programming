53f2cd74b6f0446be4d96f51e009d72603f90f28
Nate Colbert <ncolbert@cpp.edu>
2023-09-13 21:45:14 -0700

    Added conditions, print statements, and extra credit requirements

--- server.py
+++ server.py
@@ -1,27 +1,57 @@
 import socket
+import threading
+import queue
 
+SERVER_NAME = "SERVER_SOCKET"
+SERVER_NUMBER = 8
+PORT = 12000
 
-def main():
-    port = 12000
-    name = "TESTSOCKET"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', port))
-    s.listen(1)
-    print("server ready")
-    while True:
-        connection, addr = s.accept()
-
-        msg = connection.recv(1024).decode()
+def handle_client(connection, q):
+        receive_msg = connection.recv(1024).decode()
+        print("Message received!")
         
-        decoded = msg.split(",")
+        decoded = receive_msg.split(",")
         string_data = decoded[0]
         int_data = int(decoded[1])
-        print(string_data)
-        print(int_data)
-        print(type(string_data))
-        print(type(int_data))
-        msg = None
 
+        print(f"Client name: {string_data}")
+        print(f"Server Name: {SERVER_NAME}")
+
+        print(f"Client Number: {int_data}")
+        print(f"Server Number: {SERVER_NUMBER}")
+        print(f"Sum of Client and Server numbers: {int_data + SERVER_NUMBER}")
+
+        send_msg = f"{SERVER_NAME},{SERVER_NUMBER}"
+
+        connection.send(send_msg.encode())
+        print("Sending message to client!")
+
+        connection.close()
+        print("Connection has been closed!")
+
+        q.put(int_data) 
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind(('', PORT))
+    s.listen(1)
+    print(f"Listening on port: {PORT}")
+
+    while True:
+
+        connection, addr = s.accept()
+        print("Connection established!")
+        
+        print("Passing to new thread...")
+        q = queue.Queue()
+        threading.Thread(target=handle_client(connection, q)).start()
+
+        int_data = q.get()
+
+
+        if int_data < 1 or int_data > 100:
+            break
 
 if __name__ == "__main__":
     main()
